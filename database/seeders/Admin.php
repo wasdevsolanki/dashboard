01a94603f0eda2d79d5815d449c5017b6ba4a57c
unknown <washuramk@gmail.com>
2022-11-01 15:15:28 +0500

    initial commit

---
+++ database/seeders/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Admin extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('password'),
+            'is_admin' => 1
+        ]);
+    }
+}
